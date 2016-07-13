50e2da7dfe1341e6d9fbedcff0e2af4d62bd5f19
Evan Irving-Pease <ekirving@hotmail.com>
2016-07-13 15:03:05 +0100

    tidy up of gatk pipeline

--- pipeline-luigi-gatk.py
+++ pipeline-luigi-gatk.py
@@ -134,7 +134,7 @@ class Curl_Download(luigi.Task):
         print "====== cURL Downloading file ======"
 
 
-class PairedEnd_Fastq(luigi.Task):
+class Sample_PairedEnd_Fastq(luigi.Task):
     """
     Fetches the two paired-end FASTQ files for a given sample ID
     """
@@ -164,7 +164,7 @@ class PairedEnd_Fastq(luigi.Task):
 
         print "====== Downloaded Paired-end FASTQ ======"
 
-class Genome_Fasta(luigi.Task):
+class Reference_Genome_Fasta(luigi.Task):
     """
     Fetches the reference genome
     """
@@ -188,14 +188,14 @@ class Genome_Fasta(luigi.Task):
 
         print "====== Downloaded genome FASTA ======"
 
-class Samtools_Fasta_Index(luigi.Task):
+class Samtools_Faidx(luigi.Task):
     """
     Builds the samtools fasta index for the reference genome, needed for downstream CRAM files
     """
     genome = luigi.Parameter()
 
     def requires(self):
-        return Genome_Fasta(self.genome)
+        return Reference_Genome_Fasta(self.genome)
 
     def output(self):
         return luigi.LocalTarget("fasta/" + self.genome + ".fa")
@@ -207,14 +207,14 @@ class Samtools_Fasta_Index(luigi.Task):
 
         print "====== Built the samtools FASTA index ======"
 
-class Bwa_Index(luigi.Task):
+class Bwa_Index_Bwtsw(luigi.Task):
     """
     Builds the BWA index for the reference genome, needed for performing alignments
     """
     genome = luigi.Parameter()
 
     def requires(self):
-        return Genome_Fasta(self.genome)
+        return Reference_Genome_Fasta(self.genome)
 
     def output(self):
         extensions = ['amb', 'ann', 'bwt', 'pac', 'sa']
@@ -236,8 +236,8 @@ class Bwa_Mem(luigi.Task):
     genome = luigi.Parameter()
 
     def requires(self):
-        return [PairedEnd_Fastq(self.sample),
-                Bwa_Index(self.genome)]
+        return [Sample_PairedEnd_Fastq(self.sample),
+                Bwa_Index_Bwtsw(self.genome)]
 
     def output(self):
         return luigi.LocalTarget("sam/" + self.sample + ".sam")
@@ -262,7 +262,7 @@ class Bwa_Mem(luigi.Task):
 
         print "====== Aligned the FASTQ using BWA ======"
 
-class Convert_Sam_Sorted_Bam(luigi.Task):
+class Samtools_Sort_Bam(luigi.Task):
     """
     Convert an uncompressed SAM file into BAM format, and sort it
     """
@@ -290,7 +290,7 @@ class Convert_Sam_Sorted_Bam(luigi.Task):
 
         print "===== Converted SAM file to BAM ======"
 
-class Index_Bam(luigi.Task):
+class Samtools_Index_Bam(luigi.Task):
     """
     Create an index for the BAM file, for fast random access
     """
@@ -301,7 +301,7 @@ class Index_Bam(luigi.Task):
         return luigi.LocalTarget("bam/" + self.sample + ".bam.bai")
 
     def requires(self):
-        return Convert_Sam_Sorted_Bam(self.sample, self.genome)
+        return Samtools_Sort_Bam(self.sample, self.genome)
 
     def run(self):
         run_cmd(["samtools1.3",
@@ -311,7 +311,7 @@ class Index_Bam(luigi.Task):
 
         print "==== Indexing CRAM ===="
 
-class Remove_Duplicates_BAM(luigi.Task):
+class Picard_MarkDuplicates(luigi.Task):
     """
     Remove PCR duplicates, so we don't overestimate coverage
     """
@@ -319,7 +319,7 @@ class Remove_Duplicates_BAM(luigi.Task):
     genome = luigi.Parameter()
 
     def requires(self):
-        return Index_Bam(self.sample, self.genome)
+        return Samtools_Index_Bam(self.sample, self.genome)
 
     def output(self):
         return luigi.LocalTarget("bam/" + self.sample + ".rmdup.bam")
@@ -335,201 +335,34 @@ class Remove_Duplicates_BAM(luigi.Task):
 
         print "==== Removed Duplicates from BAM ===="
 
-# class Convert_Bam_Cram(luigi.Task):
+# class GATK_Variant_Call(luigi.Task):
 #     """
-#     Convert a BAM file into the smaller CRAM format
-#     """
-#     sample = luigi.Parameter()
-#     genome = luigi.Parameter()
-#
-#     def requires(self):
-#         return [Index_Bam(self.sample, self.genome),
-#                 Samtools_Fasta_Index(self.genome)]
-#
-#     def output(self):
-#         return luigi.LocalTarget("cram/" + self.sample + ".cram")
-#
-#     def run(self):
-#         # perform the BAM -> CRAM conversion
-#         cram = run_cmd(["samtools1.3",
-#                         "view",
-#                         "-@", MAX_CPU_CORES,                  # number of cores
-#                         "-C",                                 # output a CRAM file
-#                         "-T", "fasta/" + self.genome + ".fa", # reference genome
-#                         "bam/" + self.sample + ".bam"])       # input BAM file
-#
-#         # save the CRAM file
-#         with self.output().open('w') as fout:
-#             fout.write(cram)
-#
-#         print "===== Converted BAM file to CRAM ======"
-#
-# class Index_Cram(luigi.Task):
-#     """
-#     Create an index for the CRAM file, for fast random access
-#     """
-#     sample = luigi.Parameter()
-#     genome = luigi.Parameter()
-#
-#     def output(self):
-#         return luigi.LocalTarget("cram/" + self.sample + ".cram.crai")
-#
-#     def requires(self):
-#         return Convert_Bam_Cram(self.sample, self.genome)
-#
-#     def run(self):
-#         run_cmd(["samtools1.3",
-#                  "index",
-#                  "cram/" + self.sample + ".cram"])
-#
-#         print "==== Indexing CRAM ===="
-#
-# class Samtools_MPileup(luigi.Task):
-#     """
-#     Convert the BAM file into a pileup
+#     Convert the BAM file into a BCF
 #     """
 #     sample = luigi.Parameter()
 #     genome = luigi.Parameter()
 #
 #     def requires(self):
-#         return Remove_Duplicates_BAM(self.sample, self.genome)
+#         return Picard_MarkDuplicates(self.sample, self.genome)
 #
 #     def output(self):
-#         return luigi.LocalTarget("pileup/" + self.sample + ".pileup")
+#         return luigi.LocalTarget("bcf/" + self.sample + ".bcf")
 #
 #     def run(self):
-#         pileup = run_cmd(["samtools1.3",
-#                           "mpileup",
-#                           "-o",  self.output().path,            # output location
-#                           "-f", "fasta/" + self.genome + ".fa", # reference genome
-#                           "bam/" + self.sample + ".rmdup.bam"]) # input BAM file
+#         bcf = run_cmd(["samtools1.3",
+#                        "mpileup",
+#                        "-g",                                 # call genotypes
+#                        "-o", self.output().path,             # output location
+#                        "-f", "fasta/" + self.genome + ".fa", # reference genome
+#                        "bam/" + self.sample + ".rmdup.bam"]) # input BAM file
 #
 #         # TODO can't use output pipe because multithreading casues server to crash due to buffering hundreds of GB in RAM
 #
-#         # save the pileup file
+#         # # save the BCF file
 #         # with self.output().open('w') as fout:
-#         #     fout.write(pileup)
+#         #     fout.write(bcf)
 #
-#         print "===== Converted CRAM file to pileup ======="
-#
-# class Coverage_Stats(luigi.Task):
-#     """
-#     Parse the pileup and compute depth of coverage statistics
-#
-#     NB. Method is very memory hungry
-#     """
-#     sample = luigi.Parameter()
-#     genome = luigi.Parameter()
-#
-#     def requires(self):
-#         return Samtools_MPileup(self.sample, self.genome)
-#
-#     def output(self):
-#         return luigi.LocalTarget("pileup/" + self.sample + ".pileup.stats")
-#
-#     def run(self):
-#
-#         # parse the pileup and load the coverage data into memory (expensive!!)
-#         coverage = numpy.loadtxt("pileup/" + self.sample + ".pileup", dtype=int, delimiter=' ', usecols=[3])
-#
-#         # make a dictionary
-#         stats = {}
-#
-#         # calcluate the 1st and 3rd quartiles
-#         (stats['q1'], stats['q2']) = numpy.percentile(coverage, [25, 75])
-#
-#         # seralize the dictionary for later use
-#         with self.output().open('w') as fout:
-#             fout.write(json.dumps(stats))
-#
-#         print "===== Calcualted depth of coverage ======="
-
-
-class Convert_Bam_Bcf(luigi.Task):
-    """
-    Convert the BAM file into a BCF
-    """
-    sample = luigi.Parameter()
-    genome = luigi.Parameter()
-
-    def requires(self):
-        return Remove_Duplicates_BAM(self.sample, self.genome)
-
-    def output(self):
-        return luigi.LocalTarget("bcf/" + self.sample + ".bcf")
-
-    def run(self):
-        bcf = run_cmd(["samtools1.3",
-                       "mpileup",
-                       "-g",                                 # call genotypes
-                       "-o", self.output().path,             # output location
-                       "-f", "fasta/" + self.genome + ".fa", # reference genome
-                       "bam/" + self.sample + ".rmdup.bam"]) # input BAM file
-
-        # TODO can't use output pipe because multithreading casues server to crash due to buffering hundreds of GB in RAM
-
-        # # save the BCF file
-        # with self.output().open('w') as fout:
-        #     fout.write(bcf)
-
-        print "===== Converted BAM file to BCF ======="
-
-class Convert_Bcf_Vcf(luigi.Task):
-    """
-    Convert the BCF file into a VCF
-    """
-    sample = luigi.Parameter()
-    genome = luigi.Parameter()
-
-    def requires(self):
-        return Convert_Bam_Bcf(self.sample, self.genome)
-
-    def output(self):
-        return luigi.LocalTarget("vcf/" + self.sample + ".vcf")
-
-    def run(self):
-        vcf = run_cmd(["bcftools",
-                        "call",
-                        "-c",                           # consensus caller
-                        "-o", self.output().path,       # output location
-                        "-O", "v",                      # output uncompressed VCF
-                        "bcf/" + self.sample + ".bcf"]) # input BCF file
-
-        # TODO can't use output pipe because multithreading casues server to crash due to buffering hundreds of GB in RAM
-
-        # # save the VCF file
-        # with self.output().open('w') as fout:
-        #     fout.write(vcf)
-
-        print "===== Called variant sites ======="
-
-class Site_Frequency_Spectrum(luigi.Task):
-    """
-    Produce the site frequency spectrum, based on genotype calls from bcftools
-    """
-    samples = luigi.ListParameter()
-    genome = luigi.Parameter()
-
-    def requires(self):
-        for sample in self.samples:
-            yield Bcftools_Call(sample, self.genome)
-
-    def output(self):
-        return luigi.LocalTarget("fsdata/" + self.genome + ".data")
-
-    def run(self):
-
-        # log everything to file
-        logging.basicConfig(filename="fsdata/" + self.genome + ".log", level=logging.DEBUG)
-
-        # generate the frequency spectrum
-        fsdata = generate_frequency_spectrum(self.samples, WILD_THRESHOLD)
-
-        # save the fsdata file
-        with self.output().open('w') as fout:
-            fout.write(fsdata)
-
-        print "===== Generated the site frequency spectrum  ======="
+#         print "===== Converted BAM file to BCF ======="
 
 class Custom_Genome_Pipeline(luigi.Task):
     """
@@ -537,7 +370,7 @@ class Custom_Genome_Pipeline(luigi.Task):
     """
 
     def requires(self):
-        return Site_Frequency_Spectrum(SAMPLES, GENOME)
+        return Picard_MarkDuplicates(SAMPLES, GENOME)
 
 if __name__=='__main__':
     luigi.run()
