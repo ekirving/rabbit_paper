b7b7cdadf13676f7b5d8878e8c09e72e10e738c5
Evan Irving-Pease <ekirving@hotmail.com>
2016-07-17 16:50:09 +0100

    lots of updates to vcd parser

--- vcfparser.py
+++ vcfparser.py
@@ -29,6 +29,7 @@ def extract_variant_sites(population, samples, variants):
         while header.startswith("##"):
             header = infile.readline()
 
+        # get the column headers
         columns = header.split()
 
         for line in infile:
@@ -36,24 +37,32 @@ def extract_variant_sites(population, samples, variants):
             # convert string to list
             locus = line.split()
 
+            # index by chromosome and position
+            site = (locus[CHROM], locus[POS])
+
             # skip low quality sites
-            if 'LowQual' in locus[FILTER]:
-                # TODO logging
+            if locus[QUAL] != '.' and float(locus[QUAL]) < 30:
+                logging.debug('{}\t{}\tLowQual\t{}'.format(population, site, locus[QUAL]))
                 continue
 
-            # get the reference and alternatice alleles
+            # get the reference allele
             ref = locus[REF]
-            alt = locus[ALT].replace('.', ref)  # TODO check output from haplotype caller
-            # <NON_REF>
-            # T,<NON_REF>
+
+            # get the alternatve allele, deal with the stupid <NON_REF> notation used by BP_RESOLUTION
+            alt_list = locus[ALT].split(',').remove('<NON_REF>')
+
+            # skip polyallelic sites
+            if len(alt_list) > 1:
+                logging.debug('{}\t{}\tPolyAllelic\t{}/{}'.format(population, site, ref, alt_list))
+                continue
+
+            # resolve empty list issue
+            alt = ref if not alt_list else alt[0]
 
             # skip indels
             if len(ref) > 1 or len(alt) > 1:
-                # TODO logging
+                logging.debug('{}\t{}\tInDel\t{}/{}'.format(population, site, ref, alt))
                 continue
-
-            # index by chromosome and position
-            site = (locus[CHROM], locus[POS])
 
             if site not in variants:
                 # initialise site locus dictionary
@@ -64,21 +73,40 @@ def extract_variant_sites(population, samples, variants):
                 if allele not in variants[site]:
                     variants[site][allele] = defaultdict(int)
 
+            # get the indices for the genotype column (e.g. GT:AD:DP:GQ:PL:SB)
+            format = locus[FORMAT].split(':')
+
+            GT = format.index('GT')
+            DP = format.index('DP')
+            GQ = format.index('GQ')
+
             # populate the variant dictionary
             for sample in samples:
 
                 # get the genotype for the sample
-                genotype = locus[columns.index(sample)][:3]
+                genotype = locus[columns.index(sample)].split(':')
+
+                # skip low coverage samples
+                if int(genotype[DP]) < 5:
+                    logging.debug('{}\t{}\tLowCoverage\t{}\t{}'.format(population, site, sample, genotype[DP]))
+                    continue
+
+                # skip low quality samples
+                if int(genotype[GQ]) < 30:
+                    logging.debug('{}\t{}\tLowCoverage\t{}\t{}'.format(population, site, sample, genotype[DP]))
+                    continue
 
                 # count the observed alleles
-                if genotype == '0/0':
+                if genotype[GT] == '0/0':
                     # homozygous reference
                     variants[site][ref][population] += 2
-                elif genotype == '0/1':
+
+                elif genotype[GT] == '0/1':
                     # heterozygous
                     variants[site][ref][population] += 1
                     variants[site][alt][population] += 1
-                elif genotype == '1/1':
+
+                elif genotype[GT] == '1/1':
                     # homozygous alternate
                     variants[site][alt][population] += 2
 
@@ -96,6 +124,10 @@ def generate_frequency_spectrum(populations):
     # parse each of the populations and extract the variant sites
     for pop in populations:
         extract_variant_sites(pop, populations[pop], variants)
+
+    # TODO skip het sites in outgroup
+    # TODO skip hom sites
+    # TODO skip sites with < 5 sample coverage
 
     pp = pprint.PrettyPrinter(depth=6)
     pp.pprint(variants)
