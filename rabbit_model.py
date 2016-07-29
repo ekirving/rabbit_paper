f529bc886571ddddcd166c51852d3a64ea2ae265
Evan Irving-Pease <ekirving@hotmail.com>
2016-07-29 15:46:43 +0100

    testing code for PDF generation

--- rabbit_model.py
+++ rabbit_model.py
@@ -2,7 +2,7 @@ import dadi
 import numpy
 
 # Parse the data file to generate the data dictionary
-dd = dadi.Misc.make_data_dict('./fsdata/OryCun2.0.data')
+dd = dadi.Misc.make_data_dict('./fsdata/all-pops.data')
 
 data = dadi.Spectrum.from_data_dict(dd, ['DOM', 'WLD-FRE'], [16, 14], polarized=True)
 
@@ -40,27 +40,30 @@ p0 = [2,0.1,0.2,0.2]
 # Make the extrapolating version of our demographic model function.
 func_ex = dadi.Numerics.make_extrap_log_func(func)
 
-# Perturb our parameters before optimization. This does so by taking each
-# parameter a up to a factor of two up or down.
-p0 = dadi.Misc.perturb_params(p0, fold=1, upper_bound=upper_bound,
-                              lower_bound=lower_bound)
-# Do the optimization. By default we assume that theta is a free parameter,
-# since it's trivial to find given the other parameters. If you want to fix
-# theta, add a multinom=False to the call.
-# The maxiter argument restricts how long the optimizer will run. For real
-# runs, you will want to set this value higher (at least 10), to encourage
-# better convergence. You will also want to run optimization several times
-# using multiple sets of intial parameters, to be confident you've actually
-# found the true maximum likelihood parameters.
-# print('Beginning optimization ************************************************')
-popt = dadi.Inference.optimize_log(p0, data, func_ex, pts_l,
-                                   lower_bound=lower_bound,
-                                   upper_bound=upper_bound,
-                                   verbose=len(p0), maxiter=20)
+# # Perturb our parameters before optimization. This does so by taking each
+# # parameter a up to a factor of two up or down.
+# p0 = dadi.Misc.perturb_params(p0, fold=1, upper_bound=upper_bound,
+#                               lower_bound=lower_bound)
+# # Do the optimization. By default we assume that theta is a free parameter,
+# # since it's trivial to find given the other parameters. If you want to fix
+# # theta, add a multinom=False to the call.
+# # The maxiter argument restricts how long the optimizer will run. For real
+# # runs, you will want to set this value higher (at least 10), to encourage
+# # better convergence. You will also want to run optimization several times
+# # using multiple sets of intial parameters, to be confident you've actually
+# # found the true maximum likelihood parameters.
+# # print('Beginning optimization ************************************************')
+# popt = dadi.Inference.optimize_log(p0, data, func_ex, pts_l,
+#                                    lower_bound=lower_bound,
+#                                    upper_bound=upper_bound,
+#                                    verbose=len(p0), maxiter=20)
+#
+# # The verbose argument controls how often progress of the optimizer should be
+# # printed. It's useful to keep track of optimization process.
+# print('Finshed optimization **************************************************')
 
-# The verbose argument controls how often progress of the optimizer should be
-# printed. It's useful to keep track of optimization process.
-print('Finshed optimization **************************************************')
+
+popt = [0.01083985, 0.14224231, 0.0041322, 0.1030695]
 
 print('Best-fit parameters: {0}'.format(popt))
 
@@ -75,54 +78,55 @@ print('Optimal value of theta: {0}'.format(theta))
 
 # Plot a comparison of the resulting fs with the data.
 import pylab
-pylab.figure()
-dadi.Plotting.plot_2d_comp_multinom(model, data, vmin=1, resid_range=3,
-                                    pop_ids =('W','D'))
-# This ensures that the figure pops up. It may be unecessary if you are using
-# ipython.
-pylab.show()
-# Save the figure
+fig = pylab.figure(1)
+
+dadi.Plotting.plot_2d_comp_multinom(model, data, vmin=1, resid_range=3, fig_num=1)
+
+# pylab.show()
+
+fig.savefig('test.pdf')
+
 
 # # ----------------------------------------------------------------------------------------------------------------------
 #
-func_mig=dadi.Demographics2D.IM
-#ns = (n1,n2)
-#params = (s,nu1,nu2,T,m12,m21)
-
-upper_bound_1 = [1, 100, 100, 10, 3, 3]
-lower_bound_1 = [0, 1e-2, 1e-2, 0, 0, 0]
-p1 = [0.5, 2, 0.1, 0.2, 0.2, 0.2]
-
-func_ex_mig = dadi.Numerics.make_extrap_log_func(func_mig)
-
-# for i in range(0,9):
-p1 = dadi.Misc.perturb_params(p1, fold=1, upper_bound=upper_bound_1,
-                              lower_bound=lower_bound_1)
-
-print('Beginning optimization ************************************************')
-popt1 = dadi.Inference.optimize_log(p1, data, func_ex_mig, pts_l,
-                                   lower_bound=lower_bound_1,
-                                   upper_bound=upper_bound_1,
-                                   # verbose=len(p1),
-                                    maxiter=10)
-# The verbose argument controls how often progress of the optimizer should be
-# printed. It's useful to keep track of optimization process.
-print('Finshed optimization **************************************************')
-
-print('Best-fit parameters: {0}'.format(popt1))
-
-# Calculate the best-fit model AFS.
-model_mig = func_ex_mig(popt1, ns, pts_l)
-# Likelihood of the data given the model AFS.
-ll_model_mig = dadi.Inference.ll_multinom(model_mig, data)
-print('Maximum log composite likelihood: {0}'.format(ll_model_mig))
-# The optimal value of theta given the model.
-theta = dadi.Inference.optimal_sfs_scaling(model_mig, data)
-print('Optimal value of theta: {0}'.format(theta))
-
-pylab.figure()
-dadi.Plotting.plot_2d_comp_multinom(model_mig, data, vmin=1, resid_range=3,
-                                    pop_ids =('W','D'))
-# This ensures that the figure pops up. It may be unecessary if you are using
-# ipython.
-pylab.show()
+# func_mig=dadi.Demographics2D.IM
+# #ns = (n1,n2)
+# #params = (s,nu1,nu2,T,m12,m21)
+#
+# upper_bound_1 = [1, 100, 100, 10, 3, 3]
+# lower_bound_1 = [0, 1e-2, 1e-2, 0, 0, 0]
+# p1 = [0.5, 2, 0.1, 0.2, 0.2, 0.2]
+#
+# func_ex_mig = dadi.Numerics.make_extrap_log_func(func_mig)
+#
+# # for i in range(0,9):
+# p1 = dadi.Misc.perturb_params(p1, fold=1, upper_bound=upper_bound_1,
+#                               lower_bound=lower_bound_1)
+#
+# print('Beginning optimization ************************************************')
+# popt1 = dadi.Inference.optimize_log(p1, data, func_ex_mig, pts_l,
+#                                    lower_bound=lower_bound_1,
+#                                    upper_bound=upper_bound_1,
+#                                    # verbose=len(p1),
+#                                     maxiter=10)
+# # The verbose argument controls how often progress of the optimizer should be
+# # printed. It's useful to keep track of optimization process.
+# print('Finshed optimization **************************************************')
+#
+# print('Best-fit parameters: {0}'.format(popt1))
+#
+# # Calculate the best-fit model AFS.
+# model_mig = func_ex_mig(popt1, ns, pts_l)
+# # Likelihood of the data given the model AFS.
+# ll_model_mig = dadi.Inference.ll_multinom(model_mig, data)
+# print('Maximum log composite likelihood: {0}'.format(ll_model_mig))
+# # The optimal value of theta given the model.
+# theta = dadi.Inference.optimal_sfs_scaling(model_mig, data)
+# print('Optimal value of theta: {0}'.format(theta))
+#
+# pylab.figure()
+# dadi.Plotting.plot_2d_comp_multinom(model_mig, data, vmin=1, resid_range=3,
+#                                     pop_ids =('W','D'))
+# # This ensures that the figure pops up. It may be unecessary if you are using
+# # ipython.
+# pylab.show()
