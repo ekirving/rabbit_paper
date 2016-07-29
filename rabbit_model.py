2ccce0203ce49175da23121fda8087b37801e02a
Evan Irving-Pease <ekirving@hotmail.com>
2016-07-29 15:59:38 +0100

    testing

--- rabbit_model.py
+++ rabbit_model.py
@@ -76,15 +76,20 @@ print('Maximum log composite likelihood: {0}'.format(ll_model))
 theta = dadi.Inference.optimal_sfs_scaling(model, data)
 print('Optimal value of theta: {0}'.format(theta))
 
+
 # Plot a comparison of the resulting fs with the data.
 import pylab
+
+# Turn interactive plotting off
+pylab.ioff()
+
 fig = pylab.figure(1)
 
 dadi.Plotting.plot_2d_comp_multinom(model, data, vmin=1, resid_range=3, fig_num=1)
 
-# pylab.show()
-
 fig.savefig('test.pdf')
+
+pylab.close(fig)
 
 
 # # ----------------------------------------------------------------------------------------------------------------------
