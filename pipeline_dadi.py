6ff9009c6a4cf69a9ba8a3d8b9a2d5ebac57025d
Evan Irving-Pease <ekirving@hotmail.com>
2016-08-04 14:31:34 +0100

    lots of updates to DadiOptimizeLogParams

--- pipeline_dadi.py
+++ pipeline_dadi.py
@@ -5,6 +5,7 @@
 import matplotlib; matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import luigi, dadi, numpy, pylab, random
+import pickle
 
 # import the custom pipelines
 from pipeline_gatk import *
@@ -74,114 +75,277 @@ class DadiSpectrum(luigi.Task):
         fs.to_file(self.output().path)
 
 
-class DadiOptimizeLogParams(luigi.Task):
+# class DadiOptimizeLogParams(luigi.Task):
+#     """
+#     Optimise the paramaters for the given model
+#     """
+#     group = luigi.Parameter()
+#     pop1 = luigi.Parameter()
+#     pop2 = luigi.Parameter()
+#     model = luigi.Parameter(default="split_mig")
+#     n = luigi.IntParameter()
+#
+#     def requires(self):
+#         return DadiSpectrum(self.group, self.pop1, self.pop2)
+#
+#     def output(self):
+#         return luigi.LocalTarget("pdf/dadi.{0}_{1}_{2}_{3}.fs.pdf".format(self.pop1, self.pop2, self.model, self.n))
+#
+#     def run(self):
+#
+#         # reset the log buffer (just in case)
+#         log_buffer.log = []
+#
+#         # load the frequency spectrum
+#         fs = dadi.Spectrum.from_file(self.input().path)
+#
+#         # TODO test the effect of narrowing the grid
+#         # These are the grid point settings will use for extrapolation.
+#         pts_l = [10, 50, 60]
+#
+#         # get the demographic model to test
+#         func = getattr(dadi.Demographics2D, self.model)
+#
+#         # nu1: Size of population 1 after split.
+#         # nu2: Size of population 2 after split.
+#         # T: Time in the past of split (in units of 2*Na generations)
+#         # m: Migration rate between populations (2*Na*m)
+#
+#         # TODO parameterise these
+#         # The upper_bound and lower_bound lists are for use in optimization.
+#         upper_bound = [100, 100, 3, 10]
+#         lower_bound = [1e-4, 1e-4, 0, 0]
+#
+#         # randomly generated starting values within the bounding ranges
+#         p0 = [random.uniform(lower_bound[i], upper_bound[i]) for i in range(0, len(upper_bound))]
+#
+#         # Make the extrapolating version of our demographic model function.
+#         func_ex = dadi.Numerics.make_extrap_log_func(func)
+#
+#         # Do the optimization...
+#         popt = dadi.Inference.optimize_log(p0, fs, func_ex, pts_l,
+#                                            lower_bound=lower_bound,
+#                                            upper_bound=upper_bound,
+#                                            maxiter=DADI_MAX_ITER)
+#
+#         # Calculate the best-fit model AFS.
+#         model = func_ex(popt, fs.sample_sizes, pts_l)
+#
+#         # Likelihood of the data given the model AFS.
+#         ll_model = dadi.Inference.ll_multinom(model, fs)
+#
+#         # The optimal value of theta given the model.
+#         theta = dadi.Inference.optimal_sfs_scaling(model, fs)
+#
+#         # collate all the data for logging
+#         data = [self.pop1, self.pop2, self.n] + popt + [ll_model, theta] + log_buffer.log
+#
+#         # add an entry to to log
+#         with open("fsdata/{0}_{1}_{2}_{3}.tsv".format(self.pop1, self.pop2, self.model), "a") as tsv:
+#             tsv.write("\t".join(str(x).strip("\n") for x in data) + "\n")
+#
+#         # save the figure as a PDF
+#         fig = plt.figure(1)
+#         dadi.Plotting.plot_2d_comp_multinom(model, fs, vmin=1, resid_range=3, fig_num=1)
+#         fig.savefig(self.output()[1].path)
+#         plt.close(fig)
+#
+#         # # estimate of mutation rate... probably totally wrong
+#         # mu = 1.25e-8
+#         # mu = 1e-3         # rate for microsatelites in rabbits (Surridge et al., 1999)
+#         # mu = 1.622e-9     # rate for rabbits based on assumed Oryctolagus-Lepus split time of 11.8 MYA (Carneiro et al 2009:596)
+#         # mu = 1.74e-9      # (Carneiro et al., 2011)
+#         # mu = 2.02-2.35e-9 # mutations per site per generation, which is similar to but slightly lower than estimates in mice (3.4-4.1e-9; (Carneiro et al., 2012)
+#         # mu = 1.18e-6      # (Sousa et al., 2013)  μ
+#
+#         # # theta=4*Ne*mu
+#         # # Ne=theta/(4*mu)
+#         # # reference effective population size of the ancestral population (kind of)
+#         # Ne = theta / (4 * mu)
+#         #
+#         # # Ne / theta for population 1
+#         # theta1=popt[0]*theta
+#         # N1=popt[0]*Ne
+#         #
+#         # # Ne / theta for population 2
+#         # theta2=popt[1]*theta
+#         # N2 = popt[1] * Ne
+#         #
+#         # #T=2*Ne*t
+#         # #t=T/(2*Ne)
+#         # # This the time in generations
+#         # time=popt[2]*(2*Ne)
+#         #
+#         # print "mu={}".format(mu)
+#         # print "Ne={}".format(Ne)
+#         # print "theta1={}".format(theta1)
+#         # print "N1={}".format(N1)
+#         # print "theta2={}".format(theta2)
+#         # print "N2={}".format(N2)
+#         # print "time={}".format(time)
+
+
+class DadiModelOptimizeParams(luigi.Task):
     """
-    Optimise the paramaters for the given model
+    Optimise the log likelihood of the model paramaters for the given frequency spectrum
     """
     group = luigi.Parameter()
     pop1 = luigi.Parameter()
     pop2 = luigi.Parameter()
-    model = luigi.Parameter(default="split_mig")
+
+    model = luigi.Parameter()
+    grid_size = luigi.ListParameter()
+    upper_bound = luigi.ListParameter()
+    lower_bound = luigi.ListParameter()
+    param_start = luigi.ListParameter(significant=False)
+
     n = luigi.IntParameter()
 
     def requires(self):
         return DadiSpectrum(self.group, self.pop1, self.pop2)
 
     def output(self):
-        return luigi.LocalTarget("pdf/dadi.{0}_{1}_{2}_{3}.fs.pdf".format(self.pop1, self.pop2, self.model, self.n))
+        return luigi.LocalTarget("fsdata/{0}_{1}_{2}_{3}.opt".format(self.pop1, self.pop2, self.model, self.n))
 
     def run(self):
 
-        # reset the log buffer (just in case)
-        log_buffer.log = []
-
         # load the frequency spectrum
         fs = dadi.Spectrum.from_file(self.input().path)
-
-        # TODO test the effect of narrowing the grid
-        # These are the grid point settings will use for extrapolation.
-        pts_l = [10, 50, 60]
+        ns = fs.sample_sizes
 
         # get the demographic model to test
         func = getattr(dadi.Demographics2D, self.model)
 
-        # nu1: Size of population 1 after split.
-        # nu2: Size of population 2 after split.
-        # T: Time in the past of split (in units of 2*Na generations)
-        # m: Migration rate between populations (2*Na*m)
+        # Make the extrapolating version of our demographic model function.
+        func_ex = dadi.Numerics.make_extrap_log_func(func)
 
-        # TODO parameterise these
-        # The upper_bound and lower_bound lists are for use in optimization.
-        upper_bound = [100, 100, 3, 10]
-        lower_bound = [1e-4, 1e-4, 0, 0]
+        # keep a list of the optimal params
+        p_best = []
 
-        # randomly generated starting values within the bounding ranges
-        p0 = [random.uniform(lower_bound[i], upper_bound[i]) for i in range(0, len(upper_bound))]
+        # start with the randon value passed to this task
+        p_start = self.param_start
+
+        # run the optimisation many times
+        for i in range(0, DADI_MAX_ITER):
+
+            # Perturb our parameters before optimization. This does so by taking each
+            # parameter a up to a factor of two up or down.
+            p_perturb = dadi.Misc.perturb_params(p_start,
+                                                 fold=1,
+                                                 upper_bound=self.upper_bound,
+                                                 lower_bound=self.lower_bound)
+
+            print('Beginning optimization: {} ************************************************'.format(i+1))
+
+            # do the optimization...
+            p_opt = dadi.Inference.optimize_log(p_perturb, fs, func_ex, self.grid_size,
+                                                lower_bound=self.lower_bound,
+                                                upper_bound=self.upper_bound,
+                                                verbose=20,
+                                                maxiter=DADI_MAX_ITER)
+
+            print('Finshed optimization: {}  **************************************************'.format(i+1))
+
+            # reset the log buffer
+            log_buffer.log = []
+
+            # Calculate the best-fit model AFS.
+            model = func_ex(p_opt, ns, self.grid_size)
+
+            # Likelihood of the data given the model AFS.
+            ll_model = dadi.Inference.ll_multinom(model, fs)
+
+            # The optimal value of theta given the model.
+            theta = dadi.Inference.optimal_sfs_scaling(model, fs)
+
+            # get the buffered warnings
+            warnings = " ".join(log_buffer.log)
+
+            # we only care about non-masked data
+            if "Model is masked" in warnings:
+                print(warnings)
+
+            else:
+                print('Maximum log composite likelihood: {0}'.format(ll_model))
+                print('Optimal value of theta: {0}'.format(theta))
+
+                # record the best fitting params for this run
+                p_best.append([ll_model, theta] + list(p_opt))
+
+                # sort the params (largest ll_model first)
+                p_best.sort(reverse=True)
+
+            try:
+                # run the optimisation again, starting from the best fit we've seen so far
+                p_start = p_best[0][2:]
+            except IndexError:
+                # fall back on the params we started with
+                p_start = self.param_start
+
+        # save the list of optimal params by pickeling them in a file
+        with self.output().open('w') as fout:
+            pickle.dump(p_best, fout)
+
+
+class DadiModelMaximumLikelihood(luigi.Task):
+    """
+    Find the maximum log likelihood parameters for a given model, and plot the resulting spectrum
+    """
+    group = luigi.Parameter()
+    pop1 = luigi.Parameter()
+    pop2 = luigi.Parameter()
+
+    model = luigi.Parameter()
+    grid_size = luigi.ListParameter()
+    upper_bound = luigi.ListParameter()
+    lower_bound = luigi.ListParameter()
+
+    def requires(self):
+        for n in range(0, DADI_MAX_ITER):
+            # randomly generate starting params, within the bounding ranges
+            param_start = [random.uniform(self.lower_bound[i], self.upper_bound[i]) for i in range(0, len(self.upper_bound))]
+
+            # find the optimal params
+            yield DadiModelOptimizeParams(self.group, self.pop1, self.pop2, self.model, self.grid_size, self.upper_bound,
+                                          self.lower_bound, param_start, n)
+
+    def output(self):
+        return luigi.LocalTarget("pdf/dadi.{0}_{1}_{2}.pdf".format(self.pop1, self.pop2, self.model))
+
+    def run(self):
+
+        # make a master list of optimal params
+        p_best = []
+
+        for opt in self.input():
+            with opt.open('r') as fin:
+                # unpickle the optimal params
+                p_best += pickle.load(fin)
+
+        # sort the params (largest ll_model first)
+        p_best.sort(reverse=True)
+
+        # get the params with the maximum log likelihood, from all ~1e6 iterations
+        p_opt = p_best[0][2:]
+
+        # load the frequency spectrum
+        fs = dadi.Spectrum.from_file("fsdata/{0}_{1}_{2}.fs".format(self.group, self.pop1, self.pop2))
+        ns = fs.sample_sizes
+
+        # get the demographic model to test
+        func = getattr(dadi.Demographics2D, self.model)
 
         # Make the extrapolating version of our demographic model function.
         func_ex = dadi.Numerics.make_extrap_log_func(func)
 
-        # Do the optimization...
-        popt = dadi.Inference.optimize_log(p0, fs, func_ex, pts_l,
-                                           lower_bound=lower_bound,
-                                           upper_bound=upper_bound,
-                                           maxiter=DADI_MAX_ITER)
-
         # Calculate the best-fit model AFS.
-        model = func_ex(popt, fs.sample_sizes, pts_l)
+        model = func_ex(p_opt, ns, self.grid_size)
 
-        # Likelihood of the data given the model AFS.
-        ll_model = dadi.Inference.ll_multinom(model, fs)
-
-        # The optimal value of theta given the model.
-        theta = dadi.Inference.optimal_sfs_scaling(model, fs)
-
-        # collate all the data for logging
-        data = [self.pop1, self.pop2, self.n] + popt + [ll_model, theta] + log_buffer.log
-
-        # add an entry to to log
-        with open("fsdata/{0}_{1}_{2}_{3}.tsv".format(self.pop1, self.pop2, self.model), "a") as tsv:
-            tsv.write("\t".join(str(x).strip("\n") for x in data) + "\n")
-
-        # save the figure as a PDF
+        # plot the figure
         fig = plt.figure(1)
-        dadi.Plotting.plot_2d_comp_multinom(model, fs, vmin=1, resid_range=3, fig_num=1)
-        fig.savefig(self.output()[1].path)
+        dadi.Plotting.plot_2d_comp_multinom(model, fs, fig_num=1)
+        fig.savefig(self.output().path)
         plt.close(fig)
-
-        # # estimate of mutation rate... probably totally wrong
-        # mu = 1.25e-8
-        # mu = 1e-3         # rate for microsatelites in rabbits (Surridge et al., 1999)
-        # mu = 1.622e-9     # rate for rabbits based on assumed Oryctolagus-Lepus split time of 11.8 MYA (Carneiro et al 2009:596)
-        # mu = 1.74e-9      # (Carneiro et al., 2011)
-        # mu = 2.02-2.35e-9 # mutations per site per generation, which is similar to but slightly lower than estimates in mice (3.4-4.1e-9; (Carneiro et al., 2012)
-        # mu = 1.18e-6      # (Sousa et al., 2013)  μ
-
-        # # theta=4*Ne*mu
-        # # Ne=theta/(4*mu)
-        # # reference effective population size of the ancestral population (kind of)
-        # Ne = theta / (4 * mu)
-        #
-        # # Ne / theta for population 1
-        # theta1=popt[0]*theta
-        # N1=popt[0]*Ne
-        #
-        # # Ne / theta for population 2
-        # theta2=popt[1]*theta
-        # N2 = popt[1] * Ne
-        #
-        # #T=2*Ne*t
-        # #t=T/(2*Ne)
-        # # This the time in generations
-        # time=popt[2]*(2*Ne)
-        #
-        # print "mu={}".format(mu)
-        # print "Ne={}".format(Ne)
-        # print "theta1={}".format(theta1)
-        # print "N1={}".format(N1)
-        # print "theta2={}".format(theta2)
-        # print "N2={}".format(N2)
-        # print "time={}".format(time)
 
 
 class CustomDadiPipeline(luigi.WrapperTask):
@@ -191,7 +355,16 @@ class CustomDadiPipeline(luigi.WrapperTask):
 
     def requires(self):
 
-        yield SiteFrequencySpectrum('all-pops', GENOME)
+        model = 'split_mig'
+        grid_size = [10,50,60]
+        upper_bound = [100, 100, 3, 10]
+        lower_bound = [1e-2, 1e-2, 0, 0]
+
+        yield DadiModelMaximumLikelihood('all-pops', 'DOM', 'WLD-FRE')
+
+
+
+        # yield SiteFrequencySpectrum('all-pops', GENOME)
 
         # for n in range(0, 10000):
         #     yield DadiOptimizeLogParams('all-pops', 'DOM', 'WLD-FRE', n)
