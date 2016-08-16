f44509262ddd1164eae31654280ca5f6b5f2d648
Evan Irving-Pease <ekirving@hotmail.com>
2016-08-16 10:17:09 +0100

    bug fix

--- rabbit_model3.py
+++ rabbit_model3.py
@@ -11,12 +11,12 @@ pts_l = [40,50,60]
 func = dadi.Demographics2D.IM
 
 # grid to optimise over
-grid = dadi.Inference.index_exp[0.0001:0.9999:100j,  # s: Size of pop 1 after split. (Pop 2 has size 1-s.)
-                                0.01:100:100j,       # nu1: Final size of pop 1.
-                                0.01:100:100j,       # nu2: Final size of pop 2.
-                                0:3:100j,            # T: Time in the past of split (in units of 2*Na generations)
-                                0:10:100j,           # m12: Migration from pop 2 to pop 1 (2*Na*m12)
-                                0:10:100j]           # m21: Migration from pop 1 to pop 2
+grid = dadi.Inference.index_exp[0.0001:0.9999:10j,  # s: Size of pop 1 after split. (Pop 2 has size 1-s.)
+                                0.01:100:10j,       # nu1: Final size of pop 1.
+                                0.01:100:10j,       # nu2: Final size of pop 2.
+                                0:3:10j,            # T: Time in the past of split (in units of 2*Na generations)
+                                0:10:10j,           # m12: Migration from pop 2 to pop 1 (2*Na*m12)
+                                0:10:10j]           # m21: Migration from pop 1 to pop 2
 
 # Make the extrapolating version of our demographic model function.
 func_ex = dadi.Numerics.make_extrap_log_func(func)
