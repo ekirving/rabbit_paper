59e0611724c9e69b39c1cecf35d3543cac169507
Evan Irving-Pease <ekirving@hotmail.com>
2017-06-28 17:39:02 +0100

    improved map

--- rscript/plot-map.R
+++ rscript/plot-map.R
@@ -1,12 +1,10 @@
 require('maps')
 require('mapdata')
-# install.packages(c('maps','mapdata', 'scales'))
 
 setwd("/Users/Evan/Dropbox/Documents/Oxford/DPhil/Papers/TREE paper/Figs")
 
 pts <- read.table("map-coordinates2.txt", header=T, sep="\t", comment.char='')
 pts$color <- sapply(pts$color, as.character)
-# cols <- c('#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f')
 
 # setup the legend data
 legend <- unique(pts[c('date','color')])
@@ -15,27 +13,42 @@ legend <- legend[with(legend, order(date)), ]
 par(xpd=TRUE)
 
 # native range color
-col1 <- 'red'
+col1 <- '#fdbf6f'
+
+# fetch the French regions
+data(franceMapEnv)
 
 # draw the map
-map("worldHires", xlim=c(-11,19.5),ylim=c(38,55))
+map("world", xlim=c(-11,19.5),ylim=c(38,55))
 
 # color the native range
-map("world", region=c('Portugal', "Spain(?!:.+)"), fill=TRUE, add=TRUE, col=col1)
+regs <- map("france", namesonly=TRUE, plot=FALSE)
+map('france', regions=regs[c(74,79,80,86:88,91:94, 96:110,113)], fill=TRUE, col=col1, border=NA, add=TRUE)
+map("world", region=c('Portugal', "Spain(?!:.+)"), fill=TRUE, col=col1, add=TRUE)
 
+# add the sample locations
 points(pts$longitude, pts$latitude, pch=19, col=pts$color, cex=2)  #plot my sample sites
-legend('topright', inset=c(-0.2,-0.05), legend=legend$date, col=legend$color, cex=1.3, pch=19, title="Century")
+
+# add the legend
+legend('topright', inset=c(-0.3,-0.05), legend=legend$date, col=legend$color, cex=1, pch=19, title="Dates")
+
+# -------------
+# nams1 <- map("france", namesonly=TRUE, plot=FALSE)
+# map("france")
+# map('france', regions=nams1[c(74,79,80,86:88,91:94, 96:110,113)], fill=TRUE, col=col1, border=col1, add=T)
+# map('france', regions=nams1[c(74)], fill=TRUE, col='orange', add=T,border='orange')
+# map('france', regions=nams1[c(79)], fill=TRUE, col='orange', add=T,border='orange')
 
 
 # ------------
-library(splancs)
-xy <- cbind(x=runif(100), y=runif(100))
-plot(xy)
-poly <- getpoly() # this gets the polygon on-screen
-plot(xy)
-polygon(poly)
-io <- inout(xy, poly)
-# this returns a logical vector for points in the polygon
-points(xy[io,], pch=16, col="blue")
+# library(splancs)
+# xy <- cbind(x=runif(100), y=runif(100))
+# plot(xy)
+# poly <- getpoly() # this gets the polygon on-screen
+# plot(xy)
+# polygon(poly)
+# io <- inout(xy, poly)
+# # this returns a logical vector for points in the polygon
+# points(xy[io,], pch=16, col="blue")
 
 # -----------
